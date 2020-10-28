1bcda129d809c65ef6266bb3c955bf5370234c05
Natasha Schmid <njschmid@uw.edu>
2020-10-27 22:40:15 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c81897a25d16cda4822a473d6659719.js"
+  "/precache-manifest.e86a0c4ebe387cea34227a6d0a1dae87.js"
 );
 
 self.addEventListener('message', (event) => {
